6e0358d49252b12d318b261c6c5839fe6c8d3781
Abhash.Kumar <Abhash.Kumar@ext.dazn.com>
2026-04-11 16:33:27 +0530

    changed expense_ui

---
+++ app/src/main/java/com/mydomain/expensetracker/enums/TransactionType.kt
@@ -0,0 +1,7 @@
+package com.mydomain.expensetracker.enums
+
+enum class TransactionType(val text :String) {
+    CREDIT("Credit"),
+    DEBIT("Debit"),
+    SPLIT("Split")
+}
--- app/src/main/java/com/mydomain/expensetracker/screens/ExpenseScreen.kt
+++ app/src/main/java/com/mydomain/expensetracker/screens/ExpenseScreen.kt
@@ -3,6 +3,7 @@ package com.mydomain.expensetracker.screens
 import android.os.Build
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,16 +11,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.CurrencyRupee
 import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.Receipt
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -34,7 +34,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,6 +46,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.mydomain.expensetracker.Routes
 import com.mydomain.expensetracker.database.AppDatabase
+import com.mydomain.expensetracker.enums.TransactionType
 import com.mydomain.expensetracker.ui.theme.ExpenseTrackerTheme
 import com.mydomain.expensetracker.viewModelFactories.ExpenseScreenViewModelFactory
 import com.mydomain.expensetracker.viewmodel.ExpenseScreenViewModel
@@ -66,6 +68,7 @@ fun ExpenseScreen(navController: NavController) {
     val description = expenseScreenViewModel.description.collectAsState();
     val list = expenseScreenViewModel.list
     val showDatePicker = expenseScreenViewModel.showDatePicker.collectAsState()
+    val transactionType = expenseScreenViewModel.transactionType.collectAsState()
 
     LaunchedEffect(expenseScreenViewModel.toastDescription) {
         expenseScreenViewModel.toastDescription.collect { description ->
@@ -95,12 +98,15 @@ fun ExpenseScreen(navController: NavController) {
                     contentDescription = "Cancel"
                 )
             }
-            Button(onClick = {
-                expenseScreenViewModel.saveExpense()
-                navController.navigate(Routes.HomeScreen.route)
-            }) {
-                Text(text = "Save")
-            }
+            ExpenseButton(
+                {
+                    expenseScreenViewModel.saveExpense()
+                    navController.navigate(Routes.HomeScreen.route)
+                },
+                "Save",
+                contentColor = White,
+                containerColor = Color.Gray,
+            )
         }
         Row(
             modifier = Modifier
@@ -115,7 +121,7 @@ fun ExpenseScreen(navController: NavController) {
             )
         }
         Column(
-            modifier = Modifier.padding(start = 20.dp),
+            modifier = Modifier.padding(start = 20.dp, end = 20.dp),
             verticalArrangement = Arrangement.spacedBy(40.dp),
         ) {
             Row(horizontalArrangement = Arrangement.spacedBy(5.dp))
@@ -176,25 +182,61 @@ fun ExpenseScreen(navController: NavController) {
             }
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(20.dp),
-                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(text = "Date:", fontSize = 30.sp)
                 Button(
                     onClick = {
                         expenseScreenViewModel.toggleShowDatePicker()
-                    },
+                    }, modifier = Modifier.height(50.dp),
                     shape = RoundedCornerShape(5.dp),
-                ) {
-                    Row(horizontalArrangement = Arrangement.SpaceBetween)
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Gray,
+                        containerColor = White
+                    ),
+                    border = BorderStroke(1.dp, Color.Gray)
+                )
+                {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    )
                     {
-                        Icon(
-                            imageVector = Icons.Default.DateRange,
-                            contentDescription = "Choose a date"
-                        )
                         Text(text = expenseScreenViewModel.getDate())
+                        Icon(Icons.Default.DateRange, contentDescription = "Select Date")
+
                     }
                 }
+
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            )
+            {
+                ExpenseButton(
+                    {
+                        expenseScreenViewModel.setTransactionType(TransactionType.CREDIT.text)
+                    },
+                    TransactionType.CREDIT.text,
+                    contentColor = if (transactionType.value != TransactionType.CREDIT.text) Color.Gray else White,
+                    containerColor = if (transactionType.value != TransactionType.CREDIT.text) White else Color.Gray,
+                )
+                ExpenseButton(
+                    {
+                        expenseScreenViewModel.setTransactionType(TransactionType.DEBIT.text)
+                    },
+                    TransactionType.DEBIT.text,
+                    contentColor = if (transactionType.value != TransactionType.DEBIT.text) Color.Gray else White,
+                    containerColor = if (transactionType.value != TransactionType.DEBIT.text) White else Color.Gray,
+                )
+                ExpenseButton(
+                    {
+                        expenseScreenViewModel.setTransactionType(TransactionType.SPLIT.text)
+                    },
+                    TransactionType.SPLIT.text,
+                    contentColor = if (transactionType.value != TransactionType.SPLIT.text) Color.Gray else White,
+                    containerColor = if (transactionType.value != TransactionType.SPLIT.text) White else Color.Gray,
+                )
+
             }
             if (showDatePicker.value) {
                 val datePickerState = rememberDatePickerState(initialSelectedDate = LocalDate.now())
@@ -220,6 +262,28 @@ fun ExpenseScreen(navController: NavController) {
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun ExpenseButton(
+    functionalities: () -> Unit,
+    text: String,
+    contentColor: Color,
+    containerColor: Color
+) {
+    Button(
+        onClick = {
+            functionalities()
+        }, shape = RoundedCornerShape(5.dp),
+        colors = ButtonDefaults.buttonColors(
+            contentColor = contentColor,
+            containerColor = containerColor
+        ),
+        border = BorderStroke(1.dp, Color.Gray)
+    ) {
+        Text(text)
     }
 }
 
